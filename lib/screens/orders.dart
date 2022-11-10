b32afb53fb1576c8f24e87ebf6743b6802cc515e
monafhorany <monafhorany@gmail.com>
2022-11-10 17:54:52 +0300

    to be con

--- lib/screens/orders.dart
+++ lib/screens/orders.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart';
 import 'package:perllamoda/screens/order_details.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class Orders extends StatefulWidget {
   const Orders({super.key, required SharedPreferences prefs});
@@ -23,27 +28,95 @@ class _OrdersState extends State<Orders> {
 
   @override
   Widget build(BuildContext context) {
-    int orderId = 300;
     return Scaffold(
       appBar: AppBar(title: const Text("orders")),
-      body: Center(
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[],
-          ),
+      body: SafeArea(
+        child: FutureBuilder<Response>(
+          future: http.get(Uri.parse(
+              "${dotenv.env["API_URL"]!}/order/fetchAllNewOrder")), // a previously-obtained Future<String> or null
+          builder: (BuildContext context, AsyncSnapshot<Response> snapshot) {
+            if (snapshot.hasData) {
+              List<dynamic> resData =
+                  List<dynamic>.from(json.decode(snapshot.data!.body));
+              // final resData = jsonDecode(snapshot.data!.body);
+              return ListView.builder(
+                itemCount: resData.length,
+                physics: const ScrollPhysics(),
+                // padding: const EdgeInsets.all(8),
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    key: ValueKey(index.toString()),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: ListTile(
+                                leading: CircleAvatar(
+                                  radius: 30,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    widthFactor: 1,
+                                    child: Text(
+                                        resData[index]?["user"]?["name"] ??
+                                            "N/A",
+                                        textAlign: TextAlign.center),
+                                  ),
+                                ),
+                                title: Text(resData[index]["id"]?.toString() ??
+                                    "Empty"),
+                                subtitle: Text(resData[index]["ordered_by"]),
+                                trailing: const Icon(Icons.local_shipping),
+                                onTap: () {
+                                  Navigator.pushNamed(
+                                      context, OrderDetails.routeName,
+                                      arguments: ScreenArguments(
+                                          resData[index]["id"]));
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                  child: Column(children: [
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                ),
+              ]));
+            } else {
+              return Center(
+                child: Column(
+                  children: const [
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: CircularProgressIndicator(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 36),
+                      child: Text('Awaiting result...',
+                          style: TextStyle(fontSize: 30)),
+                    ),
+                  ],
+                ),
+              );
+            }
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(
-            context,
-            OrderDetails.routeName,
-            arguments: ScreenArguments(orderId),
-          );
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
